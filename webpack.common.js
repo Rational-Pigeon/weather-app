c81d1294d3ae55baea5e4c873739e4414ce094b1
Rational-Pigeon <y.nematzade.74@gmail.com>
2024-10-07 15:10:12 +0330

    Initial commit

---
+++ webpack.common.js
@@ -0,0 +1,47 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        filename: "main.js",
+        path: path.resolve(__dirname, "dist"),
+        clean: true,
+    },
+    devServer: {
+        watchFiles: ["./src/template.html"],
+    },
+    plugins: [new HtmlWebpackPlugin({
+        template: "./src/template.html",
+    })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.html$/i,
+                loader: "html-loader",
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif|webp)$/i,
+                type: "asset/resource",
+            },
+            {
+                loader: 'image-webpack-loader',
+                options: {
+                    webp: {
+                        quality: 75,
+                    },
+                }
+
+            },
+            {
+                test: /\.(woff2)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+};
