33cfca65d4821b11f408665a50da41c358be19bb
Rational-Pigeon <y.nematzade.74@gmail.com>
2024-10-19 22:45:24 +0330

    moved getCityName to fetch.js

--- src/dataProcessor.js
+++ src/dataProcessor.js
@@ -1,38 +1,10 @@
-import { getUserLocation, getWeatherData } from "./fetch";
+import { getUserLocation, getWeatherData, getCityName } from "./fetch";
 import urlComposer from "./url-composer";
 
 // Helper function to convert date string to day name
 function getDayName(dateString) {
     const date = new Date(dateString);
     return date.toLocaleDateString('en-US', { weekday: 'long' });
-}
-
-async function getCityName(resolvedAddress) {
-    if (isNaN(resolvedAddress.at(0))) {
-        return resolvedAddress.split(",")[0].trim();
-    }
-    else {
-        const apiKey = 'be2777ed166c4859ad32ef1b6915e120';
-        console.log(resolvedAddress);
-        const [latitude, longitude] = resolvedAddress.split(",");
-        console.log(latitude, longitude);
-        const url = `https://api.opencagedata.com/geocode/v1/json?q=${latitude}+${longitude}&key=${apiKey}`;
-
-        try {
-            const response = await fetch(url);
-
-            if (!response.ok) {
-                throw new Error(`Error: ${response.status} - ${response.statusText}`);
-            }
-
-            const data = await response.json();
-            const city = data.results[0].components.city || data.results[0].components.town || data.results[0].components.village;
-            return city;
-        } catch (error) {
-            console.error('Failed to get city name:', error);
-            return { error: error.message };
-        }
-    }
 }
 
 async function processWeatherData(response) {
--- src/fetch.js
+++ src/fetch.js
@@ -28,3 +28,31 @@ export async function getWeatherData(url) {
         return { error: error.message };
     }
 }
+
+export async function getCityName(resolvedAddress) {
+    if (isNaN(resolvedAddress.at(0))) {
+        return resolvedAddress.split(",")[0].trim();
+    }
+    else {
+        const apiKey = 'be2777ed166c4859ad32ef1b6915e120';
+        console.log(resolvedAddress);
+        const [latitude, longitude] = resolvedAddress.split(",");
+        console.log(latitude, longitude);
+        const url = `https://api.opencagedata.com/geocode/v1/json?q=${latitude}+${longitude}&key=${apiKey}`;
+
+        try {
+            const response = await fetch(url);
+
+            if (!response.ok) {
+                throw new Error(`Error: ${response.status} - ${response.statusText}`);
+            }
+
+            const data = await response.json();
+            const city = data.results[0].components.city || data.results[0].components.town || data.results[0].components.village;
+            return city;
+        } catch (error) {
+            console.error('Failed to get city name:', error);
+            return { error: error.message };
+        }
+    }
+}
